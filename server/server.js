2c5d126a074ffd0891fe37c41b833b06e3c0584a
Dev <devgoel2004@gmail.com>
2023-10-22 12:16:42 +0530

    next commit

--- server/server.js
+++ server/server.js
@@ -7,18 +7,28 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
-
+const path = require("path");
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json()); //to accept JSON data
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+//Deployment
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/chat/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "chat", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
